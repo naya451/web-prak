470cdf17f55669bc82b04afa0139ffb8f51a69e5
naya451 <nabelova31@gmail.com>
2023-03-16 10:20:12 +0300

    Add generated project

---
+++ src/main/java/ru/msu/cmc/webprak/WebprakApplication.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.webprak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprakApplication.class, args);
+	}
+
+}

ffa386667b9d2e1afcfc3c90c6ec3b03a4e29050
naya451 <nabelova31@gmail.com>
2023-03-16 17:00:58 +0300

    Change creation script. Add Hibernate.

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,100 +1,87 @@
 DROP TABLE IF EXISTS Sellers CASCADE;
 CREATE TABLE Sellers (
-                           Seller_ID INT,
-                           Seller_name text,
-                           Phone text,
-                           Email text,
-                           Address text,
-                           Seller_description text,
-                           PRIMARY KEY (Seller_ID)
+                           Seller_ID SERIAL PRIMARY KEY,
+                           Seller_name TEXT NOT NULL,
+                           Phone TEXT,
+                           Email TEXT,
+                           Address TEXT,
+                           Seller_description TEXT
 );
 DROP TABLE IF EXISTS Supplies CASCADE;
 CREATE TABLE Supplies (
-                            Supply_ID INT,
-                            Supply_date date,
-                            Supply_time time,
-                            Seller_ID INT,
-                            Supply_comment text,
-                            Sum_amount INT,
-                            Seller_name text,
-                            PRIMARY KEY (Supply_ID),
-                            CONSTRAINT "FK_Supplies.Seller_ID"
-                                FOREIGN KEY (Seller_ID)
-                                    REFERENCES Sellers(Seller_ID)
+                            Supply_ID SERIAL PRIMARY KEY,
+                            Supply_date DATE NOT NULL,
+                            Supply_time TIME NOT NULL,
+                            Seller_ID INTEGER NOT NULL REFERENCES Sellers(Seller_ID) ON DELETE CASCADE,
+                            Supply_comment TEXT,
+                            Sum_amount INTEGER NOT NULL CHECK (Sum_amount > 0),
+                            Seller_name TEXT NOT NULL
 );
+
 DROP TABLE IF EXISTS Goods CASCADE;
 CREATE TABLE Goods (
-                         Good_ID INT,
-                         Good_name text,
-                         Good_type text,
-                         Availability INT,
-                         Good_size INT[3],
-                         Time_of_keeping date,
-                         Good_description text,
-                         Measurement text,
-                         PRIMARY KEY (Good_ID)
+                         Good_ID SERIAL PRIMARY KEY,
+                         Good_name TEXT NOT NULL,
+                         Good_type TEXT CHECK (Good_type = 'products'
+                                                   OR Good_type = 'clothes'
+                                                   OR Good_type = 'devices'
+                                                   OR Good_type = 'for children'
+                                                   OR Good_type = 'for pets'),
+                         Availability INTEGER NOT NULL CHECK (Availability >= 0),
+                         Good_size INTEGER[3] CHECK ( Good_size[0] > 0
+                                                          AND
+                                                      Good_size[1] > 0
+                                                          AND
+                                                      Good_size[2] > 0),
+                         Time_of_keeping DATE,
+                         Good_description TEXT,
+                         Measurement TEXT NOT NULL
 );
 DROP TABLE IF EXISTS Goods_in_supplies CASCADE;
 CREATE TABLE Goods_in_supplies (
-                                     Supply_ID INT,
-                                     Good_ID INT,
-                                     Good_amount INT,
-                                     PRIMARY KEY (Supply_ID, Good_ID),
-                                     CONSTRAINT "FK_Goods_in_supplies.Supply_ID"
-                                         FOREIGN KEY (Supply_ID)
-                                             REFERENCES Supplies(Supply_ID),
-                                     CONSTRAINT "FK_Goods_in_supplies.Good_ID"
-                                         FOREIGN KEY (Good_ID)
-                                             REFERENCES Goods(Good_ID)
+                                     Supply_ID INTEGER NOT NULL REFERENCES Supplies(Supply_ID) ON DELETE CASCADE,
+                                     Good_ID INTEGER NOT NULL REFERENCES Goods(Good_ID) ON DELETE CASCADE,
+                                     Good_amount INTEGER NOT NULL CHECK (Good_amount > 0),
+                                     PRIMARY KEY (Supply_ID, Good_ID)
 );
-DROP TABLE IF EXISTS Waehouse CASCADE;
+DROP TABLE IF EXISTS Warehouse_condition CASCADE;
 CREATE TABLE Warehouse_condition (
-                                       Place_ID INT,
-                                       Room_ID INT,
-                                       Shelf_ID INT,
-                                       Goods_type text,
+                                       Place_ID SERIAL PRIMARY KEY,
+                                       Room_ID INTEGER CHECK (Room_ID >= 0),
+                                       Shelf_ID INTEGER CHECK (Shelf_ID >= 0),
+                                       Goods_type TEXT NOT NULL CHECK (Goods_type = 'products'
+                                           OR Goods_type = 'clothes'
+                                           OR Goods_type = 'devices'
+                                           OR Goods_type = 'for children'
+                                           OR Goods_type = 'for pets'),
                                        Shelf_availability bool,
-                                       Good_ID INT,
-                                       PRIMARY KEY (Place_ID),
-                                       CONSTRAINT "FK_Warehouse_condition.Good_ID"
-                                           FOREIGN KEY (Good_ID)
-                                               REFERENCES Goods(Good_ID)
+                                       Good_ID INTEGER REFERENCES Goods(Good_ID) ON DELETE SET NULL
 );
 DROP TABLE IF EXISTS Buyers CASCADE;
 CREATE TABLE Buyers (
-                          Buyer_ID INT,
-                          Buyer_name text,
-                          Phone text,
-                          Email text,
-                          Address text,
-                          Buyer_description text,
-                          PRIMARY KEY (Buyer_ID)
+                          Buyer_ID SERIAL PRIMARY KEY,
+                          Buyer_name TEXT NOT NULL,
+                          Phone TEXT,
+                          Email TEXT,
+                          Address TEXT,
+                          Buyer_description TEXT
 );
 DROP TABLE IF EXISTS Deliveries CASCADE;
 CREATE TABLE Deliveries (
-                              Delivery_ID INT,
-                              Delivery_date date,
-                              Delivery_time time,
-                              Buyer_ID INT,
-                              Delivery_comment text,
-                              Sum_amount INT,
-                              Buyer_name text,
-                              PRIMARY KEY (Delivery_ID),
-                              CONSTRAINT "FK_Deliveries.Buyer_ID"
-                                  FOREIGN KEY (Buyer_ID)
-                                      REFERENCES Buyers(Buyer_ID)
+                              Delivery_ID SERIAL PRIMARY KEY,
+                              Delivery_date DATE NOT NULL,
+                              Delivery_time TIME NOT NULL,
+                              Buyer_ID INTEGER NOT NULL REFERENCES Buyers(Buyer_ID) ON DELETE CASCADE,
+                              Delivery_comment TEXT,
+                              Sum_amount INTEGER NOT NULL CHECK (Sum_amount > 0),
+                              Buyer_name TEXT NOT NULL
 );
 DROP TABLE IF EXISTS Goods_in_delivery CASCADE;
 CREATE TABLE Goods_in_delivery (
-                                     Delivery_ID INT,
-                                     Good_ID INT,
-                                     Good_amount INT,
-                                     PRIMARY KEY (Delivery_ID, Good_ID),
-                                     CONSTRAINT "FK_Goods_in_delivery.Delivery_ID"
-                                         FOREIGN KEY (Delivery_ID)
-                                             REFERENCES Deliveries(Delivery_ID),
-                                     CONSTRAINT "FK_Goods_in_delivery.Good_ID"
-                                         FOREIGN KEY (Good_ID)
-                                             REFERENCES Goods(Good_ID)
+                                     Delivery_ID INTEGER NOT NULL REFERENCES Deliveries(Delivery_ID) ON DELETE CASCADE,
+                                     Good_ID INTEGER NOT NULL REFERENCES Goods(Good_ID) ON DELETE CASCADE,
+                                     Good_amount INTEGER NOT NULL,
+                                     PRIMARY KEY (Delivery_ID, Good_ID)
 );
 
+-- TODO: добавить триггеры

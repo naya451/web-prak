6324d3dd1b54bf8409fd28cb397c59255fe70bee
naya451 <nabelova31@gmail.com>
2023-04-20 17:07:02 +0300

    Delete all dao and models

--- src/main/java/ru/msu/cmc/webprak/DAO/Goods_in_suppliesDAO.java
+++
@@ -1,9 +0,0 @@
-package ru.msu.cmc.webprak.DAO;
-
-import ru.msu.cmc.webprak.models.Goods_in_supplies;
-
-import java.util.List;
-
-public interface Goods_in_suppliesDAO extends CommonDAO<Goods_in_supplies, Long> {
-    List<Goods_in_supplies> getAllGoods_in_supplies();
-}
--- src/main/java/ru/msu/cmc/webprak/models/CommonEntity.java
+++
@@ -1,6 +0,0 @@
-package ru.msu.cmc.webprak.models;
-
-public interface CommonEntity<ID> {
-    ID getId();
-    void setId(ID id);
-}

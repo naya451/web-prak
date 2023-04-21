abed453067f57c8477c93509a478973d4e7ad99c
naya451 <nabelova31@gmail.com>
2023-04-21 13:20:43 +0300

    Add DAO files & change README

---
+++ src/main/java/ru/msu/cmc/webprak/DAO/BuyersDAO.java
@@ -0,0 +1,25 @@
+package ru.msu.cmc.webprak.DAO;
+
+import lombok.Builder;
+import lombok.Getter;
+import ru.msu.cmc.webprak.models.Buyers;
+
+import java.util.List;
+
+public interface BuyersDAO extends CommonDAO<Buyers, Long> {
+
+    List<Buyers> getAllBuyersByName(String BuyersName);
+    Buyers getSingleBuyersByName(String BuyersName);
+    String getYearsOfLife(Buyers Buyers);
+    List<Buyers> getByFilter(Filter filter);
+
+    @Builder
+    @Getter
+    class Filter {
+        private String name;
+    }
+
+    static Filter.FilterBuilder getFilterBuilder() {
+        return Filter.builder();
+    }
+}
